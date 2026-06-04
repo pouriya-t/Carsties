255700a254c2793fc8d3f99d15fabea51e1df22e
Pouriya <pouriya.dev@outlook.com>
2026-06-04 16:11:48 +0330

    Added advanced search

--- New folder/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function Home() {
-  return (
-    <div>
-      Content goes here
-    </div>
-  );
-}
--- frontend/web-app/next.config.ts
+++ frontend/web-app/next.config.ts
@@ -1,7 +1,19 @@
 import type { NextConfig } from "next";
+import withFlowbiteReact from "flowbite-react/plugin/nextjs";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  logging: {
+    fetches: {
+      fullUrl: true
+    }
+  },
+  images: {
+    remotePatterns: [
+      { protocol: 'https', hostname: 'cdn.pixabay.com' },
+      { protocol: 'https', hostname: 'cdn1.thecatapi.com' },
+      { protocol: 'https', hostname: 'cdn2.thecatapi.com' }
+    ]
+  }
 };
 
-export default nextConfig;
+export default withFlowbiteReact(nextConfig);
